ea84bdbb28aab8807ae37ebdd728dd0b4ec1bc8e
Pedro Pablo <pmontesarroyave@gmail.com>
2022-08-02 17:33:22 -0500

    cambiamos los libros el peliculas del studio ghibl

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,40 +1,43 @@
-const API = 'https://hapi-books.p.rapidapi.com/month/2022/7'
+const API = 'https://studio-ghibli.p.rapidapi.com/films'
 
 const content = null || document.getElementById('content')
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'a7fef55e49mshc5cc3ed3de244adp1556cdjsn77db2b97f8cf',
-		'X-RapidAPI-Host': 'hapi-books.p.rapidapi.com'
+		'X-RapidAPI-Host': 'studio-ghibli.p.rapidapi.com'
 	}
 };
 
 async function fechtData(urlApi) {
-    const response = await fetch(API, options)
+    const response = await fetch(urlApi, options)
     const data = await response.json()
     return data
 }
 
 (async () => {
     try {
-        const books = await fechtData(API)
-        let view = `
-        ${books.map(book => `
+        const films = await fechtData(API)
+        const aleatorio = parseInt(films.length * Math.random())
+        
+        console.log(aleatorio ,films.length)
+        let filmReturn = `
+        ${films.map(film => `
         <div class="group relative">
           <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${book.cover}" alt="${book.name}" class="w-full">
+            <img src="${film.image}" alt="${film.title}" class="w-full">
           </div>
           <div class="mt-4 flex justify-between">
             <h3 class="text-sm text-gray-700">
               <span aria-hidden="true" class="absolute inset-0"></span>
-              ${book.name}
+              ${film.description}
             </h3>
           </div>
         </div>
-        `).slice(0, 4).join('')}
+        `).slice(aleatorio, aleatorio + 4).join('')}
         `
-        content.innerHTML = view
+        content.innerHTML = filmReturn
 
     } catch (error) {
         console.log(error)
